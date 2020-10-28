7065dbeb2512bc74df572adf5143548976982b32
aaboungab <aaboungab@qa.com>
2020-10-28 09:25:01 +0000

    updated

--- easy/near.py
+++ easy/near.py
@@ -1,17 +1,8 @@
-a=str(input("Input first word: "))
-b=str(input("Input second word: "))
+#number of tables in times-table
+tables = 10 
 
-def near(word1, word2):
-    try:
-        if len(word1) - 1 > len(word2):
-            return False
-        for i in range(0, len(word1)):
-            matching = word1[0:i:] + word1[i + 1::]
-            if matching == word2:
-                return(True)
-        else:
-            return False
-    except:
-        print("somthing went wrong")
-
-print(str(near(a,b)))
+#defines rows
+for row in range (1, tables + 1):
+    for column in range(1, tables + 1):
+        print(row * column, end="\t")
+    print()
